c76f4cd38c84ffe851b332ddb3b0c5608b7ef5b4
Elizer Dela Cruz <elidc93two@yahoo.com>
2023-01-23 04:04:27 +0800

    UI improvements, added anime.min.js

--- online-timer/main.js
+++ online-timer/main.js
@@ -12,19 +12,21 @@ var hour = h1.innerHTML + h2.innerHTML;
 var minute = m1.innerHTML + m2.innerHTML;
 var second = s1.innerHTML + s2.innerHTML;
 
-
-console.log(hour)
-console.log(minute)
-console.log(second)
+// anime({
+//     targets: 'span',
+//     translateX: 250,
+//     rotate: '1turn',
+//     backgroundColor: '#FFF',
+//     duration: 800
+//   });
 
 setInterval(function () {
-
     if (parseInt(second) == 0) {
         if (parseInt(minute) == 0) {
             if (parseInt(hour) > 0) {
                 second = 59;
                 updateDigits('s', second)
-
+            
                 minute = 59;
                 updateDigits('m', minute)
 
@@ -32,11 +34,12 @@ setInterval(function () {
                 updateDigits('h', hour)
             }
         } else if (parseInt(minute) > 0) {
-            minute = minute - 1;
-            updateDigits('m', minute)
 
             second = 59;
             updateDigits('s', second)
+
+            minute = minute - 1;
+            updateDigits('m', minute)
         }
     } else if (parseInt(second) > 0) {
         second = second - 1;
@@ -48,6 +51,8 @@ setInterval(function () {
 function updateDigits(mode, digit) {
     var element1;
     var element2;
+    var elementsToAnimate;
+
     switch(mode) {
         case 's':
             element1 = s1;
@@ -70,10 +75,34 @@ function updateDigits(mode, digit) {
         console.log()
         element1.innerHTML = digitString[0];
         element2.innerHTML = digitString[1];
+        elementsToAnimate = `#${element1.id}, #${element2.id}`
     } else {
         element1.innerHTML = 0;
         element2.innerHTML = digit;
+        elementsToAnimate = `#${element2.id}`
+        // animateDigits(elementsToAnimate);
+        // pause(target)
     }
+    // animateDigits(elementsToAnimate);
+}
+
+function pause(target) {
+    console.log(target);
+    anime({
+        targets: target
+    }).pause();
+}
+
+function animateDigits(target) {
+    console.log(target);
+    anime({
+        targets: target,
+        // rotateX: '90',
+        easing: 'easeInOutQuad',
+        rotateX: '1turn',
+        duration: 800,
+        loop: true
+    }).play();
 }
 
 function updateTitle(hour, minute, second) {
