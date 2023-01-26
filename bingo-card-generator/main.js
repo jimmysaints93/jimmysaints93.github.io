2268d83115f2df746f78411889058a49e699318b
Elizer Dela Cruz <elidc93two@yahoo.com>
2023-01-26 20:45:19 +0800

    Added bingo card generator

---
+++ bingo-card-generator/main.js
@@ -0,0 +1,66 @@
+let a = Math.floor(Math.random() * 11)
+
+var number_rules = {
+    'b': [1,15],
+    'i': [16,30],
+    'n': [31,45],
+    'g': [46,60],
+    'o': [61,75],
+};
+
+function generateRandomNumbers(rules) {
+
+    var numbers = []
+
+    function generateUniqueRandomNumber(min, max, array) {
+        let randomNumber = Math.floor(Math.random() * (max - min) ) + min;
+        while (array.includes(randomNumber)) {
+            randomNumber = Math.floor(Math.random() * (max - min) ) + min;
+        }
+        return randomNumber;
+    } 
+    
+    for (const [key,value] of Object.entries(rules)) {
+        let min = value[0];
+        let max = value[1] + 1;
+        let i = 0;
+        let nums = [];
+
+        while (i < 5) {
+            let randomNumber = generateUniqueRandomNumber(min, max, nums);
+            if (key == 'n' & i == 2) {
+                randomNumber = "FREE"
+            }
+            nums.push(randomNumber);
+            i++;
+        }
+        numbers.push(nums);
+    }
+
+    return numbers;
+}
+
+
+function generate() {
+    let generatedNumbers = generateRandomNumbers(number_rules);
+    console.log(generatedNumbers);
+
+    let rows = document.querySelectorAll("tr.row");
+
+    for (let i = 0; i < generatedNumbers.length; i++) {
+        for (let j = 0; j < rows.length; j++) {
+            let childNode = rows[j].querySelectorAll('td')[i];
+            childNode.innerHTML=generatedNumbers[i][j];
+        }
+    }
+}
+
+generate()
+
+function download(){
+    var userAdjective = prompt("Soon. Please use screenshot for now.");
+    alert (userAdjective);
+}
+
+document.getElementById("generate").onclick = generate;
+document.getElementById("download").onclick = download;
